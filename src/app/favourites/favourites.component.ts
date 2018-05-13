ba7788e2742c5e7c7e98bde398c3e7f940c3afad
Maciej Dobosz <dobosz.ma@interia.pl>
2018-05-13 06:50:23 +0200

    Przygotowanie do dalszej pracy

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -5,21 +5,34 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
 
+interface Drinks { drinkID: number; notes: string; };
+
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
   styleUrls: ['./favourites.component.css']
 })
 export class FavouritesComponent implements OnInit {
-  user: Observable<firebase.User>;
-  items: Observable<any[]>;
+  // user: Observable<firebase.User>;
+  _db: AngularFirestore;
+  drinks: Observable<any[]>;
+
 
   constructor(public afAuth: AngularFireAuth, db: AngularFirestore) {
-    this.user = this.afAuth.authState;
-    this.items = db.collection('items').valueChanges();
+    // TODO: https://www.youtube.com/watch?v=2ciHixbc4HE
+    // this.user = this.afAuth.authState;
+    this.drinks = db.collection('/items').valueChanges();
+    this._db = db;
+    console.log(this.drinks);
+    console.log(this.drinks['drinkID']);
+    this.addTshirt(15, 'dynamic');
   }
 
   ngOnInit() {
   }
 
+  addTshirt(drinkID: number, notes: string){
+    let shirtsCollection = this._db.collection<Drinks>('items');
+    shirtsCollection.add({ drinkID: drinkID, notes: notes });
+  }
 }
